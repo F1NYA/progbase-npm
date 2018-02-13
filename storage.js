dc58eb460770e08406fa8bb3e49b35b197a55834
George Kobelev <ufolkin1@gamil.com>
2018-02-13 18:02:36 +0200

    formated promise chaines

--- storage.js
+++ storage.js
@@ -4,6 +4,8 @@ const path = require('path');
 const util = require('util');
 const crypto = require('crypto');
 
+const {join} = path;
+
 const defaultSecret = os.userInfo({encoding:'base64'}).username.toString() + os.userInfo({encoding:'base64'}).homedir.toString();
 const options = { encoding: 'utf8' };
 
@@ -29,31 +31,33 @@ const unLink = util.promisify(fs.unlink);
 
 const storage = (rootDirPath) => {
     if(!fs.existsSync(rootDirPath)) mkDir(rootDirPath)
-        .then(() => mkDir(path.join(rootDirPath,'Data')).catch(err => console.error('ERROR: creating directory ' + path.join(rootDirPath, 'Data'), err)))
+        .then(() => mkDir(join(rootDirPath,'Data'))
+              .catch(err => console.error('ERROR: creating directory ' + join(rootDirPath, 'Data'), err)))
         .catch(err => console.error('ERROR: creating directory ' + rootDirPath, err));
     return {
         setData(data, fileName = 'cache.txt', pathToDir = rootDirPath, encode = false) {
-            writeFile(path.join(pathToDir,fileName), encode ? Cipher(JSON.stringify(data)) : data, options);
+            writeFile(join(pathToDir,fileName), encode ? Cipher(JSON.stringify(data)) : data, options);
         },
         getData(fileName = 'cache', pathToDir = rootDirPath, decode = false) {
-            if(fs.existsSync(path.join(pathToDir,fileName))) {
-                const data = fs.readFileSync(path.join(pathToDir, fileName), options);
+            if(fs.existsSync(join(pathToDir,fileName))) {
+                const data = fs.readFileSync(join(pathToDir, fileName), options);
                 return decode ? JSON.parse(Decipher(data)) : data;
             } else return null;
         },
         clear() {
             if(fs.existsSync(rootDirPath)) {
-                readDir(rootDirPath).then(files => files.forEach(file => statFile(path.join(rootDirPath,file))
-                    .then(stat => stat.isFile() ?
-                        unLink(path.join(rootDirPath,file)).catch(err => console.error('ERROR: removing file ' + file, err)) :
-                        rmDir(path.join(rootDirPath,file)).then(() => rmDir(rootDirPath).catch(err => console.error('ERROR: removing root directory', err))).catch(err => console.error('ERROR: removing directory ' + file, err)))
-                    .catch(err => console.error('ERROR: stat file ' + file, err))
+                readDir(rootDirPath)
+                    .then(files => files.forEach(file => statFile(join(rootDirPath,file))
+                        .then(stat => stat.isFile() ?
+                            unLink(join(rootDirPath,file)).catch(err => console.error('ERROR: removing file ' + file, err)) :
+                            rmDir(join(rootDirPath,file)).then(() => rmDir(rootDirPath).catch(err => console.error('ERROR: removing root directory', err))).catch(err => console.error('ERROR: removing directory ' + file, err)))
+                        .catch(err => console.error('ERROR: stat file ' + file, err))
                     .catch(err => console.error('ERROR: reading directory ' + rootDirPath, err))));
                 return true;
             } else return false;
         },
         dataExists(fileName = 'data.json') {
-            return fs.existsSync(path.join(rootDirPath,'Data',fileName));
+            return fs.existsSync(join(rootDirPath,'Data',fileName));
         }
     }
 };
